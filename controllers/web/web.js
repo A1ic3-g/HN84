9709d67add26baf8620aa91103cda51b4169eccf
Dom Binks <dominic.binks@gmail.com>
2023-11-04 12:40:07 +0000

    Add controllers folder
    Added folder to store code for controllers.

---
+++ controllers/web/web.js
@@ -0,0 +1,15 @@
+function up() {
+    console.log("Up");
+}
+
+function down() {
+    console.log("Down");
+}
+
+function left() {
+    console.log("Left");
+}
+
+function right() {
+    console.log("Right");
+}
