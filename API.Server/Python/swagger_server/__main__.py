d3b9dee7db6bb290a528c03fce33349ec6381718
Alice Gowler <psyag8@exmail.nottingham.ac.uk>
2023-11-04 17:17:27 +0000

    Add cors wildcard

--- API.Server/Python/swagger_server/__main__.py
+++ API.Server/Python/swagger_server/__main__.py
@@ -3,6 +3,7 @@
 import connexion
 
 from swagger_server import encoder
+from flask_cors import CORS
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'HackPac - OpenAPI 3.0'}, pythonic_params=True)
     app.run(port=8080)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 if __name__ == '__main__':
