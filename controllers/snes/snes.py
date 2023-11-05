33f21460f818bdc122d5c8071979c10e3da6c893
Dom Binks <dominic.binks@gmail.com>
2023-11-05 01:01:17 +0000

    Add heartbeat to snes controller

--- controllers/snes/snes.py
+++ controllers/snes/snes.py
@@ -1,6 +1,8 @@
 import evdev
 import requests
 import json
+import threading
+from time import sleep
 
 def getId():
     response = requests.get("http://localhost:8080/api/v1/controller")
@@ -16,7 +18,15 @@ def postDirection(direction):
     requests.post("http://localhost:8080/api/v1/controller/"+str(id)+"/direction",
         data=json.dumps({"direction": direction}), headers={"Content-type": "application/json"})
 
+def maintain():
+    while True:
+        requests.post("http://localhost:8080/api/v1/controller/"+str(id)+"/heartbeat",
+            data=json.dumps({}), headers={"Content-type": "application/json"})
+        sleep(5)
+
+
 id = getId()
+threading.Thread(target=maintain).start()
 
 device = evdev.InputDevice('/dev/input/event16')
 
